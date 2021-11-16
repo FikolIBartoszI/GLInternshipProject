c542df67f56aa7ad403acb16d06284eb06f1ff9f
Pecuch <krzysztof.pecuch@partner.magna.com>
2021-11-16 09:08:10 +0100

    First script with main

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+def main() -> int:
+    """Simple main function
+
+    Returns:
+        int: system exit code
+    """
+    print("Hello World")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
